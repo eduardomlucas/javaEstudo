ca86eae5cdc3e3accf062db12cdcc7b3b7b38d9c
Eduardo <eduardomlucas>
2022-01-17 11:37:57 -0300

    sobrecarga de método

---
+++ eduardo/aula7/Main7.java
@@ -0,0 +1,14 @@
+package aula7;
+
+public class Main7 {
+    public static void main(String[] args) {
+        User userA = new User("Eduardo", "Lucas");
+        
+        System.out.println(userA.output());
+        System.out.println(userA.output(false));
+        
+        
+    
+        
+    }
+}
---
+++ eduardo/aula7/User.java
@@ -0,0 +1,42 @@
+package aula7;
+//classe padrão de um usuário
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    //função q vai imprimir o primeiro nome + o segundo nome
+    public String output(){
+        return firstName.toUpperCase() + " " + lastName.toUpperCase();
+    }
+
+    //o mesmo método output mas comn diferenças
+    //ao invés de pegar os dois nomes, apenas o primeiro será pego
+    //o parâmetro dentro da mesma função define a diferença do método
+    //o if vai testar a função se vai ser passada com parâmetro ou não
+    public String output(boolean showLastName){
+        if(showLastName){
+            return output();
+        }
+        else{
+            return firstName;
+        }
+    }
+}
